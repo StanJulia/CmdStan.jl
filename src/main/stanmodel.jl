1b60793fa75fe08c5bb0cb0ea035c480bbdf3c15
Rob J Goedman <goedman@mac.com>
2018-05-25 16:48:25 -0600

    Created Manifest.toml. Many trials.

--- src/main/stanmodel.jl
+++ src/main/stanmodel.jl
@@ -141,17 +141,18 @@ function Stanmodel(
   output=Output(),
   pdir::String=pwd(),
   create_dataframe=false)
-    
+  
+  println("=====> $pdir\n")
   cd(pdir)
   
-  tmpdir = Pkg.dir(pdir, "tmp")
+  tmpdir = joinpath(pdir, "tmp")
   if !isdir(tmpdir)
     mkdir(tmpdir)
   end
   
   model_file = "$(name).stan"
   if length(model) > 0
-    update_model_file(Pkg.dir(tmpdir, "$(name).stan"), strip(model))
+    update_model_file(joinpath(tmpdir, "$(name).stan"), strip(model))
   end
   
   id::Int=0
