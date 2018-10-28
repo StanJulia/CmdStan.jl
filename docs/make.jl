d22625d11e8d6fc792db3904a5fbd675f886aa8b
Rob J Goedman <goedman@mac.com>
2018-10-28 12:23:09 -0600

    Docs 0.20 updates

--- docs/make.jl
+++ docs/make.jl
@@ -15,9 +15,9 @@ makedocs(
 
 deploydocs(
     repo = "github.com/StanJulia/CmdStan.jl.git",
-    target = "build",
-    julia = "1.0",
-    osname = "linux",
-    deps = nothing,
-    make = nothing
+    #target = "build",
+    #julia = "1.0",
+    #osname = "linux",
+    # deps = nothing,
+    #make = nothing
  )
