912d8d178fe1961f78089adfb59a675d486a6c19
Oliver Dechant <16816606+oliverdechant@users.noreply.github.com>
2019-02-28 12:10:00 -0400

    optional include

---
+++ examples/Normal/normal_monitors.jl
@@ -0,0 +1,36 @@
+# example borrowed from th0br0
+using CmdStan
+using StanMCMCChain
+using MCMCChain
+
+y = collect(1:100);
+x = [3:3:300 5:5:500];
+
+
+model = """
+data {
+  int<lower = 0> N;
+  int<lower = 0> k;
+  matrix[N, k] x;
+  real<lower=0> y[N];
+}
+parameters {
+  vector[k] beta; 
+  real<lower=0> sigma;
+}
+model {
+  y ~ normal(x * beta, sigma);
+}
+""";
+
+# beta parameters are accessed using beta.[1-9+] syntax
+data = Dict("N"=>100, "k" => 2, "x" => x, "y" => y);
+stanmodel = Stanmodel(monitors = ["beta.1", "sigma"], model=model,
+          output_format=:mcmcchain);
+
+rc, sim, cnames = stan(stanmodel, data; diagnostics = false);
+
+cnames
+# 2-element Array{String,1}: "beta.1" "sigma" 
+
+describe(sim)
