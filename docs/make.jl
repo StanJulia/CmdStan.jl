7d0660756527dae7b5e00818c5b0d13c8e7a5f66
Rob J Goedman <goedman@mac.com>
2019-03-18 18:49:32 -0600

    Switching to docs job format

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,19 @@
 using Documenter, CmdStan
 
-makedocs(
-    modules = [CmdStan],
-    format = :html,
-    sitename = "StanJulia/CmdStan.jl",
-    pages = Any[
-        "Home" => "INTRO.md",
-        "Installation" => "INSTALLATION.md",
-        "Walkthrough" => "WALKTHROUGH.md",
-        "Versions" => "VERSIONS.md",
-        "Index" => "index.md"
-    ]
+makedocs( root = DOC_ROOT,
+  modules = [],
+  sitename = "StanJulia/CmdStan.jl",
+  authors = "Rob J Goedman",
+  pages = Any[
+      "Home" => "INTRO.md",
+      "Installation" => "INSTALLATION.md",
+      "Walkthrough" => "WALKTHROUGH.md",
+      "Versions" => "VERSIONS.md",
+      "Index" => "index.md"
+  ]
 )
 
 deploydocs(
-    repo = "github.com/StanJulia/CmdStan.jl.git",
+  root = DOC_ROOT,
+  repo = "github.com/StanJulia/CmdStan.jl.git",
  )
