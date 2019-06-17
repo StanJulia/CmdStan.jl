ba89008622b9e1c36260f20159444971c5f74e9e
Rob J Goedman <goedman@mac.com>
2019-06-17 15:45:26 -0600

    Added StanSamples to the StanRun example.

--- examples/StanRun/bernoulli.jl
+++ examples/StanRun/bernoulli.jl
@@ -52,6 +52,10 @@ cd(ProjDir) #do
   output_base = StanRun.default_output_base(sm)
   pipelin_cmd = StanRun.stan_cmd_and_paths(exec_path, data_path, output_base, 1)
   
+  nt = read_samples(output_base*"_chain_1.csv")
+  display(nt)
+  println()
+  
   a3d, cnames = read_stanrun_samples(output_base, "_chain")
   chns = convert_a3d(a3d, cnames, Val(:mcmcchains); start=1)
   describe(chns)
