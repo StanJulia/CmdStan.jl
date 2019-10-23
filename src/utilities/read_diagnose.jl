90d19823100f5e2822ff3f03485f23ad57842936
Rob J Goedman <goedman@mac.com>
2019-10-23 08:08:22 -0400

    Updated read_diagnose.jl

--- src/utilities/read_diagnose.jl
+++ src/utilities/read_diagnose.jl
@@ -35,33 +35,32 @@ function read_diagnose(model::Stanmodel)
         
         str = read("$(model.name)_$(res_type)_$(i).csv", String)
         sstr = split(str)
-        tdict[:stan_major_version] = [parse(Int, sstr[4])]
-        tdict[:stan_minor_version] = [parse(Int, sstr[8])]
-        tdict[:stan_patch_version] = [parse(Int, sstr[12])]
+        tdict[:stan_version] = "$(parse(Int, sstr[4])).$(parse(Int, sstr[8])).$(parse(Int, sstr[12]))"
       end
       
-      # Position sstr at the beginning of last comment line
+      # Position sstr at element with with `probability=...`
       
-      sstr_lp = sstr[79]
+      indx = findall(x -> length(x)>11 && x[1:11] == "probability", sstr)[1]
+      sstr_lp = sstr[indx]
       sstr_lp = parse(Float64, split(sstr_lp, '=')[2])
       
       if :lp in keys(tdict)
         append!(tdict[:lp], sstr_lp)
-        append!(tdict[:var_id], parse(Int, sstr[90]))
-        append!(tdict[:value], parse(Float64, sstr[91]))
-        append!(tdict[:model], parse(Float64, sstr[92]))
-        append!(tdict[:finite_dif], parse(Float64, sstr[93]))
-        append!(tdict[:error], parse(Float64, sstr[94]))
+        append!(tdict[:var_id], parse(Int, sstr[indx+11]))
+        append!(tdict[:value], parse(Float64, sstr[indx+12]))
+        append!(tdict[:model], parse(Float64, sstr[indx+13]))
+        append!(tdict[:finite_dif], parse(Float64, sstr[indx+14]))
+        append!(tdict[:error], parse(Float64, sstr[indx+15]))
       else
         
         # First time around, create value array
         
         tdict[:lp] = [sstr_lp]
-        tdict[:var_id] = [parse(Int, sstr[90])]
-        tdict[:value] = [parse(Float64, sstr[91])]
-        tdict[:model] = [parse(Float64, sstr[92])]
-        tdict[:finite_dif] = [parse(Float64, sstr[93])]
-        tdict[:error] = [parse(Float64, sstr[94])]
+        tdict[:var_id] = [parse(Int, sstr[indx+11])]
+        tdict[:value] = [parse(Float64, sstr[indx+12])]
+        tdict[:model] = [parse(Float64, sstr[indx+13])]
+        tdict[:finite_dif] = [parse(Float64, sstr[indx+14])]
+        tdict[:error] = [parse(Float64, sstr[indx+15])]
       end
     end
   end
