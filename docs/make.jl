b6cfead78b505db815a1b9a876aa9639d1ad2091
Rob J Goedman <goedman@mac.com>
2019-12-24 20:51:29 -0700

    Updated docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,28 @@
-using Documenter, CmdStan
+using CmdStan, Documenter
 
 DOC_ROOT = rel_path_cmdstan("..", "docs")
+DocDir =  rel_path("..", "docs", "src")
 
-makedocs( root = DOC_ROOT,
-  modules = [CmdStan],
-  sitename = "StanJulia/CmdStan.jl",
+page_list = Array{Pair{String, Any}, 1}();
+append!(page_list, [Pair("Home", "INTRO.md")]);
+append!(page_list, [Pair("Installation", "INSTALLATION.md")]);
+append!(page_list, [Pair("Walkthrough", "WALKTHROUGH.md")]);
+append!(page_list, [Pair("Versions", "VERSIONS.md")]);
+append!(page_list, [Pair("Functions", "index.md")]);
+
+makedocs( 
+  format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_ACTIONS")),
+  root = DOC_ROOT,
+  modules = Module[],
+  sitename = "CmdStan.jl",
   authors = "Rob J Goedman",
-  pages = Any[
-      "Home" => "INTRO.md",
-      "Installation" => "INSTALLATION.md",
-      "Walkthrough" => "WALKTHROUGH.md",
-      "Versions" => "VERSIONS.md",
-      "Index" => "index.md"
-  ]
+  pages = page_list,
 )
 
 deploydocs(
-  root = DOC_ROOT,
-  repo = "github.com/StanJulia/CmdStan.jl.git",
+    root = DOC_ROOT,
+    repo = "github.com/StanJulia/CmdStan.jl.git",
+    versions = "v#",
+    devbranch = "master",
+    push_preview = true,
  )
